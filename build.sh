0e2d5278c0e4a726c03594d2d7976ab834ac3762
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-21 06:51:12 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,15 +2,12 @@ apt update
 apt install -y ca-certificates  
 echo "deb-src https://deb.debian.org/debian stable main" |  tee -a /etc/apt/sources.list
 apt update
-apt build-dep zathura mupdf -y
-apt install wget libmupdf-dev zathura-dev gcc meson ca-certificates  -y
-version=0.4.1
+apt install wget libmupdf-dev libwlroots-dev libxml2-dev libcairo2-dev libpango1.0-dev libpng-dev librsvg2-dev   gcc cmake  meson ca-certificates binutils  -y
+version=0.5.3
 echo $version > version 
-wget https://github.com/pwmt/zathura-pdf-mupdf/archive/refs/tags/$version.tar.gz
+wget https://github.com/labwc/labwc/archive/refs/tags/$version.tar.gz
 tar -xzf $version.tar.gz
-cd zathura-pdf-mupdf-$version/
-apt-get source mupdf
-sed -i -e '/^[[:space:]]*tesseract/s/^[[:space:]]*\(tesseract\)/# \1/' -e '/^[[:space:]]*leptonica/s/^[[:space:]]*\(leptonica\)/# \1/' meson.build
-meson setup build
-cd build
-ninja
+cd labwc-$version/
+meson build/
+ninja -C build/
+strip build/labwc
